565fb7fe906193e638c2a5436fd5261620b3096e
Travis CI <noreply+travis@iamareebjamal.org>
2017-10-30 20:50:09 +0530

    chore: Initial commit

---
+++ src/App.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-app light>
+    <v-navigation-drawer
+      persistent
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      v-model="drawer"
+      enable-resize-watcher
+      app
+    >
+      <v-list>
+        <v-list-tile
+          value="true"
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-tile-action>
+            <v-icon dark v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar dark class="primary" fixed app :clipped-left="clipped">
+      <v-toolbar-side-icon @click.stop="drawer = !drawer" dark></v-toolbar-side-icon>
+      <v-btn
+        icon
+        dark
+        @click.stop="miniVariant = !miniVariant"
+      >
+        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        dark
+        @click.stop="clipped = !clipped"
+      >
+        <v-icon>web</v-icon>
+      </v-btn>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        icon
+        dark
+        @click.stop="rightDrawer = !rightDrawer"
+      >
+      </v-btn>
+    </v-toolbar>
+    <main>
+      <v-content>
+        <v-container fluid>
+          <v-slide-y-transition mode="out-in">
+            <v-layout column align-center>
+              <img src="/static/img/v.png" alt="Vuetify.js" class="mb-5">
+              <blockquote>
+                &#8220;First design the UI, then add functionality.&#8221;
+                <footer>
+                  <small>
+                    <em>&mdash;NabsZains</em>
+                  </small>
+                </footer>
+              </blockquote>
+            </v-layout>
+          </v-slide-y-transition>
+        </v-container>
+      </v-content>
+    </main>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        clipped: false,
+        drawer: true,
+        fixed: false,
+        items: [
+          { icon: 'bubble_chart', title: 'Inspire' }
+        ],
+        miniVariant: false,
+        right: true,
+        title: 'Roll-call'
+      }
+    }
+  }
+</script>
