722182f03170a0c63d8152b92cbe97831025900f
Areeb Jamal <jamal.areeb@gmail.com>
2018-04-10 22:26:52 +0530

    feat: Change menu names

--- src/components/User/DropDown.vue
+++ src/components/User/DropDown.vue
@@ -36,14 +36,6 @@
             <v-list-tile-title>Profile</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
-        <v-list-tile>
-          <v-list-tile-action>
-            <v-icon>vpn_key</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Change Password</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
         <v-divider></v-divider>
         <v-list-tile @click="logout">
           <v-list-tile-action>
